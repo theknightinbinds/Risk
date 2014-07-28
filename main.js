412fb0dbeff14207cf6d654293008b37b9dfb2da
Zack C-K <clarz016@gmail.com>
2014-07-28 11:35:14 +0100

    Added a counties overlay
    Counties overlay
    http://nemezisproject.co.uk/download/google-maps-playground/England.kmz
    added to the main.js file

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 function initialize() {
-        var mapOptions = {
-          center: new google.maps.LatLng(53.9410509, -3.2284443),
-          zoom: 6
-        };
-        var map = new google.maps.Map(document.getElementById("map-canvas"),
-            mapOptions);
-      }
-      google.maps.event.addDomListener(window, 'load', initialize);
+  var centrepoint = new google.maps.LatLng(53.9410509,-3.2284443);
+  var mapOptions = {
+    zoom: 6,
+    center: centrepoint
+  }
+
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+  var ctaLayer = new google.maps.KmlLayer({
+    url: 'http://nemezisproject.co.uk/download/google-maps-playground/England.kmz'
+  });
+  ctaLayer.setMap(map);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
