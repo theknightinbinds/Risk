33ebe7624c79d3f7471ae13129daded2d364379b
Zack C-K <clarz016@gmail.com>
2014-07-28 13:01:19 +0100

    Can now click to add markers
    Edited main.js to allow clicking for adding markers

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
+var map;
 function initialize() {
   var centrepoint = new google.maps.LatLng(53.9410509,-3.2284443);
   var mapOptions = {
     zoom: 6,
-    center: centrepoint
+    center: centrepoint,
+	disableDefaultUI: true
   }
 
-  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
+  map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  google.maps.event.addListener(map, 'click', addPoint);
   var ctaLayer = new google.maps.KmlLayer({
     url: 'http://nemezisproject.co.uk/download/google-maps-playground/England.kmz'
   });
@@ -14,3 +16,21 @@ function initialize() {
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
+
+ 
+function addPoint(event) { 
+    var marker = new google.maps.Marker({
+        position: event.latLng,
+        map: map,
+        draggable: true
+    });
+    markers.push(marker);
+    google.maps.event.addListener(marker, 'click', function() {
+        marker.setMap(null);
+        for (var i = 0, I = markers.length; i < I && markers[i] != marker; ++i);
+        markers.splice(i, 1);
+    });
+    google.maps.event.addListener(marker, 'dragend', function() {
+ 
+    });
+}
